c87bfb7735015a393f1b757efe181099b0f954fd
devmarcos <devmarcoseduardo@gmail.com>
2023-08-22 21:35:59 -0300

    feat: new image field added

--- src/dataProducts/productsData.ts
+++ src/dataProducts/productsData.ts
@@ -3,6 +3,7 @@ export interface Product {
     nome: string;
     preco: number;
     quantity: number;
+    image: string;
 }
 
 export interface ProductsByCategory {
@@ -11,28 +12,28 @@ export interface ProductsByCategory {
 
 export const productsByCategory: ProductsByCategory = {
     "🍕 Pizzas": [
-        { id: 1, nome: "Pizza Calabresa", preco: 10.0, quantity: 1 },
-        { id: 2, nome: "Pizza Frango Catupiry", preco: 15.0, quantity: 1 },
-        { id: 3, nome: "Pizza Moda da Casa", preco: 10.0, quantity: 1 },
-        { id: 4, nome: "Pizza Feijoada", preco: 15.0, quantity: 1 },
-        { id: 5, nome: "Pizza 4 Queijos", preco: 10.0, quantity: 1 },
-        { id: 6, nome: "Pizza Mussarela", preco: 15.0, quantity: 1 },
+        { id: 1, nome: "Pizza Calabresa", preco: 10.50, quantity: 1, image: "/pizzas/calabresa.jpeg" },
+        { id: 2, nome: "Pizza Frango Catupiry", preco: 15.0, quantity: 1, image: "/pizzas/frangoCatupiry.jpg" },
+        { id: 3, nome: "Pizza Moda da Casa", preco: 10.0, quantity: 1, image: "/pizzas/modaDaCasa.jpg" },
+        { id: 4, nome: "Pizza Feijoada", preco: 15.0, quantity: 1, image: "/pizzas/feijoada.jpg" },
+        { id: 5, nome: "Pizza 4 Queijos", preco: 10.0, quantity: 1, image: "/pizzas/quatroQueijos.jpg" },
+        { id: 6, nome: "Pizza Mussarela", preco: 15.0, quantity: 1, image: "/pizzas/mussarela.jpg" },
     ],
     "🍕 Pizzas Doces": [
-        { id: 1, nome: "Pizza de Banana", preco: 12.0, quantity: 1 },
-        { id: 2, nome: "Pizza Chocolate ao Leite", preco: 18.0, quantity: 1 },
-        { id: 3, nome: "Pizza Chocobana", preco: 12.0, quantity: 1 },
-        { id: 4, nome: "Pizza Dois Amores", preco: 18.0, quantity: 1 },
-        { id: 5, nome: "Pizza Prestigio", preco: 12.0, quantity: 1 },
-        { id: 6, nome: "Pizza Romeu e Julieta", preco: 18.0, quantity: 1 },
+        { id: 1, nome: "Pizza de Banana", preco: 12.0, quantity: 1, image: "" },
+        { id: 2, nome: "Pizza Chocolate ao Leite", preco: 18.0, quantity: 1, image: "" },
+        { id: 3, nome: "Pizza Chocobana", preco: 12.0, quantity: 1, image: "" },
+        { id: 4, nome: "Pizza Dois Amores", preco: 18.0, quantity: 1, image: "" },
+        { id: 5, nome: "Pizza Prestigio", preco: 12.0, quantity: 1, image: "" },
+        { id: 6, nome: "Pizza Romeu e Julieta", preco: 18.0, quantity: 1, image: "" },
     ],
     "🍺 Bebidas": [
-        { id: 1, nome: "Coca Cola 1l", preco: 12.0, quantity: 1 },
-        { id: 2, nome: "Guaraná Antática 1l", preco: 18.0, quantity: 1 },
-        { id: 3, nome: "Fanta Laranja 1l", preco: 12.0, quantity: 1 },
-        { id: 4, nome: "Kuat 1l", preco: 18.0, quantity: 1 },
-        { id: 5, nome: "Sprite Fresh", preco: 12.0, quantity: 1 },
-        { id: 6, nome: "Fanta Uva 1l", preco: 18.0, quantity: 1 },
+        { id: 1, nome: "Coca Cola 1l", preco: 12.0, quantity: 1, image: "" },
+        { id: 2, nome: "Guaraná Antática 1l", preco: 18.0, quantity: 1, image: "" },
+        { id: 3, nome: "Fanta Laranja 1l", preco: 12.0, quantity: 1, image: "" },
+        { id: 4, nome: "Kuat 1l", preco: 18.0, quantity: 1, image: "" },
+        { id: 5, nome: "Sprite Fresh", preco: 12.0, quantity: 1, image: "" },
+        { id: 6, nome: "Fanta Uva 1l", preco: 18.0, quantity: 1, image: "" },
     ],
     "🍨 Sobremesas": [
 
